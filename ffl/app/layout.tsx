d5746569985679cbd2d68f7293cbd9aa28e9144c
Adam Branscum <93352907+adambranscum@users.noreply.github.com>
2024-07-09 16:14:15 -0500

    fix script tag to async 07-09

--- ffl/app/layout.tsx
+++ ffl/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       
       <Footer />
 
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"/>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" async/>
 
       </body>
     </html>

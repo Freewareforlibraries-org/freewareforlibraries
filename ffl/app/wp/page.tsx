4344f5ebb7fbb3c2c67f10067a9a513143dbc064
Adam Branscum <93352907+adambranscum@users.noreply.github.com>
2024-08-01 15:45:37 -0500

    created wp info page

---
+++ ffl/app/wp/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div className="d-flex mt-auto mb-auto fade-in">
+    <div className="container p-4">   
+    <h1 className="display-1 text-teal-mid fw-bolder text-center">Wireless Print Hub</h1>
+    <h1 className="text-teal-mid fw-bolder text-center mt-4">What is it?</h1>
+    <p className="fs-5 text-teal-mid text-center">Introducing the ultimate solution for public libraries in need of a reliable and efficient wireless printing system - 
+        our Wireless Print Hub. This innovative product is designed to streamline the printing process, making it easier than ever for patrons to print their important 
+        documents from any device.
+    </p>
+
+    <h1 className="text-teal-mid fw-bolder text-center mt-4">Is it free?</h1>
+
+<p className="fs-5 text-teal-mid text-center">This application offers a free tier, as well as a paid option for libraries that require private hosting for their wireless printing tool.</p>
+
+<h1 className="text-teal-mid fw-bolder text-center mt-4">How do I get started?</h1>
+
+<p className="fs-5 text-teal-mid text-center">To use our free tier, click <a href="">here</a> and register for your free account! To inquire about the paid
+ tier email our webmaster at <a href="mailto:webmaster@freewareforlibraries.org">webmaster@freewareforlibraries.org</a></p>
+      </div>
+      </div>
+  )
+}
+
+export default about

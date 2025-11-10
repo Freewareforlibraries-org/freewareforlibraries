b7e19a7afb79346b18c82daf63879bedc712f5f0
Adam Branscum <93352907+adambranscum@users.noreply.github.com>
2025-11-09 22:47:41 -0600

    ugh

--- app/community-resources/community-forums/page.tsx
+++ app/community-resources/community-forums/page.tsx
@@ -5,10 +5,7 @@ const LibraryCommunities = () => {
     <div className="d-flex mt-auto mb-auto fade-in">
       <div className="container p-4">   
         <h1 className="display-1 text-teal-mid fw-bolder text-center">Communities & Forums</h1>
-      <div className="mb-3">
-          
-        </div>
-        <div className="alert alert-info border-0 shadow-sm mx-auto mb-3" style={{maxWidth: '800px'}} role="alert">
+        <div className="alert alert-info border-0 shadow-sm mx-auto mb-4" style={{maxWidth: '800px'}} role="alert">
           <p className="mb-0 text-center">
             These communities connect you with thousands of library technology professionals 
             sharing solutions, best practices, and support for IT challenges in libraries!
